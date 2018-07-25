34a70a24682312b693c868d3bf580852322a0964
Seleah <shecancodetoo@gmail.com>
2018-07-25 07:34:35 -0700

    Add function to load cat image

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
-let clickCounter = document.querySelector('.click-count'),
-	cat = document.querySelector('img');
+let clickCounter = document.querySelector('.click-count');
+clickCounter.innerText = 0;
+
+(function loadCat() {
+	let i = Math.floor(Math.random() * 8);
+	let pic = `images/${i}.jpg`;
+	document.getElementById('catpicture').src = pic;
+} ());
