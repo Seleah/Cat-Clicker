75cd3a66f7023a405874d0aebe98e92612eb9cb4
Seleah <seleah.face@gmail.com>
2018-07-25 08:05:11 -0700

    Add event listener, fix loadCat function and add changeItUp function, all to change the cat image on click but not allow for multiple clicks between image changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
-let clickCounter = document.querySelector('.click-count');
-clickCounter.innerText = 0;
+let clickCounter = document.querySelector('.click-count'),
+	count = 0;
 
-(function loadCat() {
+function loadCat() {
 	let i = Math.floor(Math.random() * 8);
 	let pic = `images/${i}.jpg`;
 	document.getElementById('catpicture').src = pic;
-} ());
+}
+
+loadCat();
+
+function changeItUp() {
+	document.removeEventListener('click', changeItUp);
+	setTimeout(function() {
+		loadCat();
+		count += 1;
+		clickCounter.innerText = `Clicks ${count}`;
+		document.addEventListener('click', changeItUp);
+	}, 3000);
+}
+
+document.addEventListener('click', changeItUp);
