c2ccb06333dd035b102b12defa4b3b428e0900c1
Seleah <seleah.face@gmail.com>
2018-07-31 13:08:29 -0700

    Build model object

--- js/app.js
+++ js/app.js
@@ -2,7 +2,51 @@
 
 /* ---------- Model ---------- */
 
-
+let model = {
+	currentCat: null,
+	cats: [
+		{
+			name: 'Oscar',
+			image: 'images/0.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Tiger',
+			image: 'images/1.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Cleo',
+			image: 'images/2.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Jack',
+			image: 'images/3.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Smokey',
+			image: 'images/4.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Milo',
+			image: 'images/5.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Chloe',
+			image: 'images/6.jpg',
+			clickCount: 0
+		},
+		{
+			name: 'Misty',
+			image: 'images/7.jpg',
+			clickCount: 0
+		}
+	]
+};
 
 
 /* --------- Octopus --------- */
